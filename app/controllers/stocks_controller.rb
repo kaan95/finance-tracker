f0a7d0e8906fd9d4f16758d343874b8620318b3d
Kaan Oezgiray <kaan.oezgiray@hotmail.de>
2018-04-15 16:06:23 +0000

    update controller + model for invalid search results

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,9 +1,22 @@
 class StocksController < ApplicationController
   
   def search
-    # store the looked up stock (from user) in @stock
-    @stock = Stock.new_from_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      # store the looked up stock (from user) in @stock
+      @stock = Stock.new_from_lookup(params[:stock])
+      # if stock symbol was correct, render this
+      if @stock
+        render 'users/my_portfolio'
+      # if @stock is nil (happens if user mistypes a symbol)   
+      else
+        flash[:danger] = "You have entered an incorrect stock ticker"
+        redirect_to my_portfolio_path
+      end
+      # if user doesnt enter anything
+    else
+      flash[:danger] = "You did not enter anything... "
+      redirect_to my_portfolio_path
+    end
   end
   
 end

3d338c5297f3f53a4a17a46056048297ad59ff61
Kaan Oezgiray <kaan.oezgiray@hotmail.de>
2018-04-26 13:37:19 +0000

    add friendship model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
   def my_portfolio
     @user = current_user
     @user_stocks = current_user.stocks
-  end  
+  end
+  
+  def my_friends
+    
+  end
 end
